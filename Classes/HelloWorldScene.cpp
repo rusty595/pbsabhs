b8d22163e1b0bb01c719cb47e7a31bafe774ebfd
Christopher McCreadie <christopher.mccreadie@staffs.ac.uk>
2015-11-18 18:20:35 +0000

    Added Touch functionality, added label that moves to touched position. Makes use of random number generation to create text colour

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -2,6 +2,11 @@
 #include "cocostudio/CocoStudio.h"
 #include "ui/CocosGUI.h"
 
+// Includes required for Pseudo Random Numbers
+#include <cstdlib>
+#include <ctime>
+
+
 USING_NS_CC;
 
 using namespace cocostudio::timeline;
@@ -35,5 +40,59 @@ bool HelloWorld::init()
 
     addChild(rootNode);
 
+	// Add the label to give us some feedback on where we have touched
+	labelTouchInfo = Label::createWithSystemFont("Touch or click on the screen", "Arial", 30);
+
+	// Set the label to be in the middle of the screen
+	labelTouchInfo->setPosition(cocos2d::Vec2(Director::getInstance()->getVisibleSize().width / 2, Director::getInstance()->getVisibleSize().height / 2));
+
+	// Create a custom event listener
+	auto touchListener = EventListenerTouchOneByOne::create();
+
+	// Assign the event methods to the event listener (known as callbacks)
+	touchListener->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchBegan, this);
+	touchListener->onTouchEnded = CC_CALLBACK_2(HelloWorld::onTouchEnded, this);
+	touchListener->onTouchMoved = CC_CALLBACK_2(HelloWorld::onTouchMoved, this);
+	touchListener->onTouchCancelled = CC_CALLBACK_2(HelloWorld::onTouchCancelled, this);
+
+	/* For more information on the eventdispatcher mechanism (and how events in Cocos work in general) go to http://www.cocos2d-x.org/wiki/EventDispatcher_Mechanism */
+	// Add the event listener to the event dispatcher
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, this);
+
+	// Finally, lets add the label to the layer (HelloWorld is a layer, NOT a scene)
+	this->addChild(labelTouchInfo);
+
+	// Finally, lets create our seed for the random number generator
+	srand(time(NULL));
+
     return true;
+}
+
+bool HelloWorld::onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event)
+{
+	// Create three random numbers that will be the colour of our text
+	GLubyte red = rand() % 255;
+	GLubyte green = rand() % 255;
+	GLubyte blue = rand() % 255;
+	// Create the colour based on our random numbers
+	cocos2d::Color3B myColour = cocos2d::Color3B(red, green, blue);
+	labelTouchInfo->setPosition(touch->getLocation());
+	labelTouchInfo->setString("You pressed HERE!");
+	labelTouchInfo->setColor(myColour);
+	return true;
+}
+
+void HelloWorld::onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* event)
+{
+	
+}
+
+void HelloWorld::onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* event)
+{
+
+}
+
+void HelloWorld::onTouchCancelled(cocos2d::Touch* touch, cocos2d::Event* event)
+{
+
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -12,8 +12,16 @@ public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
 
+	virtual bool onTouchBegan(cocos2d::Touch*, cocos2d::Event*);
+	virtual void onTouchEnded(cocos2d::Touch*, cocos2d::Event*);
+	virtual void onTouchMoved(cocos2d::Touch*, cocos2d::Event*);
+	virtual void onTouchCancelled(cocos2d::Touch*, cocos2d::Event*);
+
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+
+private:
+	cocos2d::Label* labelTouchInfo;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
